75c914bcced96cf9245d3b50555000ca78fa53bf
Peter Martin <p15martin@outlook.com>
2013-03-08 08:51:46 -0800

    refactoring to expose loading as a function call

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-var fs    = require('fs');
 
-module.exports = function requireAll(options) {
-  var files   = fs.readdirSync(options.dirname);
-  var modules = {};
+var RequireAll = function() {
+  fs = require('fs'),
+
+  loadAllModules : function (options) {
+    var files   = fs.readdirSync(options.dirname);
+    var modules = {};
+
+    files.forEach(function(file) {
+      var filepath = options.dirname + '/' + file;
+      if (fs.statSync(filepath).isDirectory()) {
+
+        if (excludeDirectory(file)) {
+          return;
+        }
+
+        modules[file] = requireAll({
+          dirname     :  filepath,
+          filter      :  options.filter,
+          excludeDirs :  options.excludeDirs,
+          dependencies : options.dependencies
+        });
+
+      } else {
+        var match = file.match(options.filter);
+        if (match) {
+          modules[file.split('.')[0]] = require(filepath).apply(this, options.dependencies);
+        }
+      }
+    });
+  },
 
   function excludeDirectory(dirname) {
     return options.excludeDirs && dirname.match(options.excludeDirs);
-  }
+  };
 
-  files.forEach(function(file) {
-    var filepath = options.dirname + '/' + file;
-    if (fs.statSync(filepath).isDirectory()) {
+  return {
+    loadAllModules : loadAllModules
+  };
+}();
 
-      if (excludeDirectory(file)) return;
-
-      modules[file] = requireAll({
-        dirname     :  filepath,
-        filter      :  options.filter,
-        excludeDirs :  options.excludeDirs,
-        dependencies : options.dependencies
-      });
-
-    } else {
-      var match = file.match(options.filter);
-      if (match) {
-        modules[file.split('.')[0]] = require(filepath).apply(this, options.dependencies);
-      }
-    }
-  });
-
-  return modules;
-};
-
+module.exports = RequireAll;
