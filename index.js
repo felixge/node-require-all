d1d3bed05407dafeab04e9b0b1c114252e7eb8c3
Peter Martin <p15martin@outlook.com>
2013-08-14 14:45:55 -0700

    Allow files to be exlcuded

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
+/*jslint node:true */
+var RequireAll = (function () {
+    'use strict';
+    
+    var fs = require('fs'),
 
-var RequireAll = function() {
-  fs = require('fs'),
+        excludeDirectory = function (excludeDirs, dirname) {
+            return excludeDirs && dirname.match(excludeDirs);
+        },
 
-  excludeDirectory = function (excludeDirs, dirname) {
-    return excludeDirs && dirname.match(excludeDirs);
-  },
+        loadAllModules = function (options) {
+            var files   = fs.readdirSync(options.dirname),
+                modules = {};
+    
+            files.forEach(function (file) {
+                if (excludeDirectory(options.excludeDirs, file)) {
+                    return;
+                }
+                
+                var filepath = options.dirname + '/' + file;
+            
+                if (fs.statSync(filepath).isDirectory()) {
+                    modules[file] = loadAllModules({
+                        dirname     :  filepath,
+                        filter      :  options.filter,
+                        excludeDirs :  options.excludeDirs,
+                        dependencies : options.dependencies
+                    });
+                } else {
+                    if (file.match(options.filter)) {
+                        modules[file.split('.')[0]] = require(filepath).apply(this, options.dependencies);
+                    }
+                }
+            });
+        };
 
-  loadAllModules = function (options) {
-    var files   = fs.readdirSync(options.dirname);
-    var modules = {};
-
-    files.forEach(function(file) {
-      var filepath = options.dirname + '/' + file;
-      if (fs.statSync(filepath).isDirectory()) {
-
-        if (excludeDirectory(options.excludeDirs, file)) {
-          return;
-        }
-
-        modules[file] = loadAllModules({
-          dirname     :  filepath,
-          filter      :  options.filter,
-          excludeDirs :  options.excludeDirs,
-          dependencies : options.dependencies
-        });
-
-      } else {
-        var match = file.match(options.filter);
-        if (match) {
-          modules[file.split('.')[0]] = require(filepath).apply(this, options.dependencies);
-        }
-      }
-    });
-  };
-
-  return {
-    loadAllModules : loadAllModules
-  };
-}();
+    return {
+        loadAllModules : loadAllModules
+    };
+}());
 
 module.exports = RequireAll;
